d6255e4262ef0fe074f16649bb654f23fa8229f0
Shivani <shivani.gkl1@gmail.com>
2023-06-12 05:32:53 -0400

    Remove unused colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        mist: "var(--mist-color)",
         silver: "var(--silver-color)",
-        primary: {
-          900: "var(--primary-color-900)",
-          DEFAULT: "var(--primary-color-800)",
-        },
         ink: "var(--ink-color)",
       },
     },
