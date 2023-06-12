b615a196c241390a7c27b2ea1e40cf1e65bf5e38
Shivani <shivani.gkl1@gmail.com>
2023-06-12 02:52:33 -0400

    Added test cases for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,81 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(() =>
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let breakpointObserver: BreakpointObserver;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatListModule,
+        MatSidenavModule,
+        MatSnackBarModule,
+      ],
       declarations: [AppComponent],
-    })
-  );
+      providers: [BreakpointObserver],
+    }).compileComponents();
+
+    breakpointObserver = TestBed.inject(BreakpointObserver);
+  });
+
+  afterEach(() => {
+    component.ngOnDestroy();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'products-management'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('products-management');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'products-management app is running!'
+  it('should show a menu button in the header when in mobile mode', () => {
+    spyOn(breakpointObserver, 'observe').and.returnValue(
+      of({ matches: true, breakpoints: {} })
     );
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.isMobile).toBe(true);
+    const menuButton = fixture.debugElement.query(
+      By.css('[data-testid="menu-button"]')
+    );
+    expect(menuButton).toBeTruthy();
+  });
+
+  it('should not show menu button in the header when in desktop mode', () => {
+    spyOn(breakpointObserver, 'observe').and.returnValue(
+      of({ matches: false, breakpoints: {} })
+    );
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.isMobile).toBe(false);
+    const menuButton = fixture.debugElement.query(
+      By.css('[data-testid="menu-button"]')
+    );
+    expect(menuButton).toBeFalsy();
   });
 });
